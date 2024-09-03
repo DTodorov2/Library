71fd80c59541b661cda0ac26ad3c6896121f6dca
DTodorov2 <danailtodorov440@gmail.com>
2024-09-03 16:24:59 +0300

    Updating add/remove book functionality

--- Library/src/Client.cpp
+++ Library/src/Client.cpp
@@ -13,12 +13,12 @@ bool Client::removeUser(std::vector<User*>& users, const std::string& username) 
 	std::cout << "Sorry, you are not allowed to remove users from the system!" << std::endl;
 }
 
-bool Client::addBook(std::vector<Book>& books) const
+bool Client::addBook(std::vector<Book>& books, int id) const
 {
 	std::cout << "Sorry, you are not allowed to add books into the system!" << std::endl;
 }
 
-bool Client::removeBook(std::vector<Book>& books) const
+bool Client::removeBook(std::vector<Book>& books, int id) const
 {
 	std::cout << "Sorry, you are not allowed to remove books from the system!" << std::endl;
 }
