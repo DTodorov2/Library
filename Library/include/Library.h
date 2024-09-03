2210dadb1e6b52835b24d3c5867d12f73babc65f
DTodorov2 <danailtodorov440@gmail.com>
2024-09-03 12:18:43 +0300

    Adding functionality

--- Library/include/Library.h
+++ Library/include/Library.h
@@ -9,6 +9,12 @@ class Library
 
 	int currentUserIndex = -1;
 
+public:
 	void login(const std::string& username, const std::string& pass);
 	void logout();
+	void open(const std::string& fileName) const; //opens a File
+	void close(/*tuka nqkuv stream priema*/) const; // closes a file
+	void save(/*tuka nqkuv stream priema*/) const;
+	void saveas(/*tuka nqkuv stream priema*/) const;
+	void help() const;
 };
